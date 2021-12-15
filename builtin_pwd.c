26ffb5423815012b3bde023b02cf7fdabd42066a
Ng Wee Hean <heanconcerto@gmail.com>
2021-12-15 17:37:18 +0800

    Added pwd built-in function

--- builtin_pwd.c
+++ builtin_pwd.c
@@ -6,14 +6,37 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:50:49 by weng              #+#    #+#             */
-/*   Updated: 2021/12/15 14:57:25 by weng             ###   ########.fr       */
+/*   Updated: 2021/12/15 17:36:15 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/* print name of current/working directory */
 int	ft_pwd(char **args)
 {
-	(void) args;
-	return (1);
+	extern int	errno;
+	char		*buf;
+	int			retval;
+
+	buf = NULL;
+	retval = 0;
+	if (args[1] != NULL)
+	{
+		errno = EINVAL;
+		perror("pwd");
+	}
+	else
+	{
+		buf = getcwd(buf, 0);
+		if (buf == NULL)
+			perror("pwd");
+		else
+		{
+			printf("%s\n", buf);
+			free(buf);
+			retval = 1;
+		}
+	}
+	return (retval);
 }
