4475a88828c689ae787ce588ee322fe7c359bfb2
Ng Wee Hean <heanconcerto@gmail.com>
2021-12-15 17:21:59 +0800

    Added exit built-in function

--- builtin_exit.c
+++ builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:50:49 by weng              #+#    #+#             */
-/*   Updated: 2021/12/15 14:59:57 by weng             ###   ########.fr       */
+/*   Updated: 2021/12/15 17:17:53 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 int	ft_exit(char **args)
 {
-	(void) args;
-	return (1);
+	extern int	errno;
+
+	if (args[1] != NULL)
+	{
+		errno = EINVAL;
+		perror("exit");
+		exit(EXIT_FAILURE);
+	}
+	else
+		exit(EXIT_SUCCESS);
 }
--- minishell.h
+++ minishell.h
@@ -6,13 +6,14 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 17:22:25 by weng              #+#    #+#             */
-/*   Updated: 2021/12/15 14:34:55 by weng             ###   ########.fr       */
+/*   Updated: 2021/12/15 17:15:00 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <errno.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <stdlib.h>
