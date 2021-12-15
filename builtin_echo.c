d0f7cfd8a950ac28e88a82cf36a0b77dc82c0bf1
Ng Wee Hean <heanconcerto@gmail.com>
2021-12-15 23:58:57 +0800

    Added echo built-in function

--- builtin_echo.c
+++ builtin_echo.c
@@ -6,14 +6,33 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:50:49 by weng              #+#    #+#             */
-/*   Updated: 2021/12/15 14:56:37 by weng             ###   ########.fr       */
+/*   Updated: 2021/12/15 23:55:46 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/* Display a line of text. */
 int	ft_echo(char **args)
 {
-	(void) args;
+	int	has_newline;
+	int	i;
+
+	has_newline = 1;
+	i = 1;
+	if (args[1] != NULL && ft_strncmp(args[1], "-n", 3) == 0)
+	{
+		has_newline = 0;
+		i = 2;
+	}
+	while (args[i] != NULL)
+	{
+		printf("%s", args[i]);
+		if (args[i + 1] != NULL)
+			printf(" ");
+		i++;
+	}
+	if (has_newline == 1)
+		printf("\n");
 	return (1);
 }
