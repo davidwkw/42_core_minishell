8f953916eea50e2b7f92a411d42dec0c784fbec7
Ng Wee Hean <heanconcerto@gmail.com>
2021-12-16 13:09:05 +0800

    Corrected pwd built-in function to accept arguments

--- builtin_pwd.c
+++ builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:50:49 by weng              #+#    #+#             */
-/*   Updated: 2021/12/15 17:36:15 by weng             ###   ########.fr       */
+/*   Updated: 2021/12/16 13:07:53 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,19 @@
 /* print name of current/working directory */
 int	ft_pwd(char **args)
 {
-	extern int	errno;
 	char		*buf;
-	int			retval;
 
-	buf = NULL;
-	retval = 0;
-	if (args[1] != NULL)
+	(void) args;
+	buf = getcwd(NULL, 0);
+	if (buf == NULL)
 	{
-		errno = EINVAL;
 		perror("pwd");
+		return (0);
 	}
 	else
 	{
-		buf = getcwd(buf, 0);
-		if (buf == NULL)
-			perror("pwd");
-		else
-		{
-			printf("%s\n", buf);
-			free(buf);
-			retval = 1;
-		}
+		printf("%s\n", buf);
+		free(buf);
+		return (1);
 	}
-	return (retval);
 }
