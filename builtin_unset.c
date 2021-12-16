50542699bf38433680e78b74a9fdb5dfe9c6133f
Ng Wee Hean <heanconcerto@gmail.com>
2021-12-16 15:59:27 +0800

    Added unset built-in function

--- builtin_unset.c
+++ builtin_unset.c
@@ -6,14 +6,45 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:50:49 by weng              #+#    #+#             */
-/*   Updated: 2021/12/15 14:59:27 by weng             ###   ########.fr       */
+/*   Updated: 2021/12/16 15:56:29 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+extern char	**environ;
+
+/*
+Removes the corresponding environment variable for each NAME given.
+
+The environment variables are removed by deleteing the corresponding
+entry from environ global variable for each NAME. However, this
+implementation is not thread safe, since no lock is used.
+*/
 int	ft_unset(char **args)
 {
-	(void) args;
+	int		len;
+	char	**env;
+
+	args++;
+	while (*args != NULL)
+	{
+		len = ft_strlen(*args);
+		env = environ;
+		while (*env != NULL)
+		{
+			if (ft_strncmp(*env, *args, len) == 0 && (*env)[len] == '=')
+			{
+				while (*env != NULL)
+				{
+					env[0] = env[1];
+					env++;
+				}
+				break ;
+			}
+			env++;
+		}
+		args++;
+	}
 	return (1);
 }
