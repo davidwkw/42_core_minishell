0cbc77130097261ee28465a7c3dd2d5927412c96
Ng Wee Hean <heanconcerto@gmail.com>
2022-01-14 11:29:40 +0800

    Amended ft_star_split to account for quoted asterisks

--- expand_star.c
+++ expand_star.c
@@ -6,7 +6,7 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 15:06:01 by kwang             #+#    #+#             */
-/*   Updated: 2022/01/14 10:28:15 by weng             ###   ########.fr       */
+/*   Updated: 2022/01/14 11:34:36 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,54 +44,76 @@ static t_list	*ft_ls(const char *dir, char hidden)
 }
 
 /* Returns 1 if 'str' matches the pattern given by 'pattern', or 0 otherwise. */
-static int	ft_str_match(char *str, char **pattern)
+static int	ft_str_match(char *str, t_list *pattern)
 {
 	int		i;
+	t_list	*node;
+	char	*content;
 	char	*addr;
 
 	i = -1;
 	addr = str;
-	while (pattern[++i] != NULL)
+	node = pattern;
+	while (node != NULL)
 	{
-		if (ft_strncmp(pattern[i], "*", 2) == 0)
+		content = node->content;
+		if (ft_strncmp(content, "*", 2) == 0)
 			addr = ft_strchr(addr, '\0');
 		else if (i == 0)
 		{
-			if (*pattern[i] == '*')
-				addr = ft_strnstr(addr, pattern[i] + 1, ft_strlen(addr));
-			else if (ft_strncmp(addr, pattern[i], ft_strlen(pattern[i])) != 0)
+			if (*content == '*')
+				addr = ft_strnstr(addr, content + 1, ft_strlen(addr));
+			else if (ft_strncmp(addr, content, ft_strlen(content)) != 0)
 				addr = NULL;
 		}
 		else
-			addr = ft_strnstr(addr, pattern[i], ft_strlen(addr));
+			addr = ft_strnstr(addr, content, ft_strlen(addr));
 		if (addr == NULL)
 			return (0);
-		addr += ft_strlen(pattern[i] + (*pattern[i] == '*'));
+		addr += ft_strlen(content + (*content == '*'));
+		node = node->next;
 	}
 	return (*addr == '\0');
 }
 
-/*
-Splits the search string by a given character. If the string begins with a *
-it is reappended to the first element of the split list. If the string ends with
-a *, the list is expanded and a single element of "*" if added to the end of the
-list. Returns the formatted list.
-*/
-static char	**ft_star_split(char *search)
+/* Auxiliary recursive function to be called by ft_star_split. */
+static t_list	*ft_star_split_aux(char *input, t_list *lst)
 {
-	char	**segments;
-	char	*temp;
+	char	*end;
+	char	*str;
 
-	segments = ft_split(search, '*');
-	if (*search == '*')
-	{
-		temp = ft_strjoin("*", segments[0]);
-		free(segments[0]);
-		segments[0] = temp;
-	}
-	if (search[ft_strlen(search) - 1] == '*')
-		ft_arradd_back(&segments, ft_strdup("*"));
-	return (segments);
+	while (*input == '*')
+		input++;
+	if (*input == '\0')
+		return (lst);
+	end = ft_strchr_unquoted(input, '*');
+	if (end != NULL)
+		str = ft_substr(input, 0, end - input);
+	else
+		str = ft_strdup(input);
+	ft_lstadd_back(&lst, ft_lstnew(str));
+	return (ft_star_split_aux(input + ft_strlen(str), lst));
+}
+
+/*
+Returns a linked list of substring, from splitting the 'input' at each
+unquoted '*' character.
+
+If 'input' begins or ends with a '*', the a node containing pointer to
+"*" string added to the front or back of the linked list respectively.
+*/
+static t_list	*ft_star_split(char *input)
+{
+	t_list	*lst;
+
+	if (*input == '\0')
+		return (NULL);
+	lst = ft_star_split_aux(input, NULL);
+	if (*input == '*')
+		ft_lstadd_front(&lst, ft_lstnew(ft_strdup("*")));
+	if (input[ft_strlen(input) - 1] == '*')
+		ft_lstadd_back(&lst, ft_lstnew(ft_strdup("*")));
+	return (lst);
 }
 
 /*
@@ -101,22 +123,22 @@ list containing the 'pattern'.
 */
 t_list	*ft_expand_star(const char *dir, char *pattern)
 {
-	char	**segments;
+	t_list	*substr;
 	t_list	*filenames;
 	t_list	*node;
 
-	segments = ft_star_split(pattern);
+	substr = ft_star_split(pattern);
 	filenames = ft_ls(dir, *pattern == '.');
 	node = filenames;
 	while (node != NULL)
 	{
-		if (ft_str_match(node->content, segments) == 0)
+		if (ft_str_match(node->content, substr) == 0)
 			ft_lst_replace_content(node, ft_strdup(""), free);
 		node = node->next;
 	}
 	if (filenames != NULL)
 		filenames = ft_lstdelempty(&filenames);
-	ft_arrclear(segments, free);
+	ft_lstclear(&substr, free);
 	if (filenames != NULL)
 		return (filenames);
 	else
