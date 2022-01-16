04021b01ac9f5bbb8e7ded872829ef4bb29eff13
Ng Wee Hean <heanconcerto@gmail.com>
2022-01-16 18:31:15 +0800

    Removed ft_tokenise_aux function

--- lexer.c
+++ lexer.c
@@ -6,7 +6,7 @@
 /*   By: weng <weng@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/25 15:32:45 by weng              #+#    #+#             */
-/*   Updated: 2022/01/16 18:12:48 by weng             ###   ########.fr       */
+/*   Updated: 2022/01/16 18:30:13 by weng             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,36 +70,28 @@ static char	*ft_next_token(const char *str)
 	}
 }
 
-/* Auxiliary function that adds one token to 'lst' at each call. */
-static t_list	*ft_tokenise_aux(char *input, t_list *lst)
-{
-	int		len;
-	char	*token;
-	char	*end;
-
-	while (*input == ' ')
-		input++;
-	if (*input == '\0')
-		return (lst);
-	end = ft_next_token(input);
-	if (ft_istoken(input, &token, 1) == 0)
-		token = ft_substr(input, 0, end - input);
-	ft_lstadd_back(&lst, ft_lstnew(strdup(token)));
-	len = ft_strlen(token);
-	free(token);
-	return (ft_tokenise_aux(input + len, lst));
-}
-
-/*
-Tokenize the input and return a linked list, with variable expansion and
-quote removal carried out.
-*/
+/* Tokenize the input and return a linked list. */
 t_list	*ft_tokenise(char *input)
 {
 	t_list	*lst;
+	char	*token;
+	char	*end;
 
 	if (ft_is_well_quoted(input) == 0 || ft_is_well_bracketed(input) == 0)
 		return (NULL);
-	lst = ft_tokenise_aux(input, NULL);
+	lst = NULL;
+	while (*input != '\0')
+	{
+		while (*input == ' ')
+			input++;
+		if (*input == '\0')
+			break ;
+		end = ft_next_token(input);
+		if (ft_istoken(input, &token, 1) == 0)
+			token = ft_substr(input, 0, end - input);
+		ft_lstadd_back(&lst, ft_lstnew(strdup(token)));
+		input += ft_strlen(token);
+		free(token);
+	}
 	return (lst);
 }
